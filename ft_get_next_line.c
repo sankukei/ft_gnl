d67e5d7aad4c62561c92c0c350abe4b6c96126c7
sanku <sankukei@gmail.com>
2024-11-29 02:26:20 +0100

    might be done soon

--- ft_get_next_line.c
+++ ft_get_next_line.c
@@ -18,7 +18,7 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-#define BUFFER_SIZE 5
+#define BUFFER_SIZE 10
 
 char	*ft_strjoin(int count, char *s1, char *s2);
 int	ft_check_buffer(char *str);
@@ -49,47 +49,81 @@ int	ft_check_buffer(char *str)
 	return (0);
 }
 
-char	*ft_strjoin(int count, char *s1, char *s2)
+char	*ft_strjoin(int count, char *s1, char *buf)
 {
 	size_t	i;
 	size_t	y;
 	char	*res;
-	i = 0;
+	i = -1;
 	y = 0;
 
-	res = malloc((count * BUFFER_SIZE) + 1);
+	count++;
+	res = malloc((ft_strlen(s1) + ft_strlen(buf)) + 1);
 	if (!res)
 		return (NULL);
-	while (i < ft_strlen(s1))
-	{
+	while (++i < ft_strlen(s1))
 		res[i] = s1[i];
-		i++;
-	}
-	while (y < ft_strlen(s2))
-	{
-		res[i++] = s2[y++];
-	}
+	while (y < ft_strlen(buf) && buf[y] != '\n')
+		res[i++] = buf[y++];
 	res[i] = '\0';
 	return (res);
 }
 
-char	*ft_get_next_line(int fd)
+char	*xd(char *dest, char *buf)
+{
+	int	i;
+	int	y;
+
+	i = 0;
+	y = 0;
+	while(buf[i] != '\n')
+		i++;
+	if (ft_check_buffer(buf))
+	{
+		dest = malloc(i + 1);
+		while(y < i)
+		{
+			dest[y] = buf[y];
+			y++;
+		}
+	}
+	return (dest);
+}
+
+char	*get_next_line(int fd)
 {
 	char	*res;
 	int	count;
-
+//	int	i;
 
 	//TODO GERER LE CAS OU BUFFER_SIZE EST SUPPERIEUR A LA LIGNE
+//	i = 0;
 	res = NULL;
 	count = 1;
 	static char	buf[BUFFER_SIZE];
-	while (read(fd, buf, BUFFER_SIZE) && !ft_check_buffer(buf))
+	//printf("BUFFER: %s\n", buf);
+	while (read(fd, buf, BUFFER_SIZE) && ft_check_buffer(buf))
 	{
 		res = ft_strjoin(count, res, buf);
 		if (!res)
 			return (NULL);
 		count++;
 	}
+	//int 	y;
+	//y = 0;
+	//y = ft_strlen(res + 1);
+	//while (res && res[y])
+	//	y++;
+	/*if (ft_check_buffer(buf))
+	{
+		while (buf[i] != '\n')
+			res[y++] = buf[i++];
+	}*/
+	if (ft_check_buffer(buf))
+		res = ft_strjoin(count, res, buf);
+	int len = ft_strlen(res);
+	res[len] = '\n';
+	//res[y] = '\n';
 	return (res);
 }
 
@@ -98,8 +132,8 @@ int main(void)
 	int	fd;
 
 	fd = open("./test", O_RDONLY);
-	printf("%s" , ft_get_next_line(fd));
-	printf("%s" , ft_get_next_line(fd));
-	printf("%s" , ft_get_next_line(fd));
+	printf("1 CALL: %s" , get_next_line(fd));
+	printf("2 CALL: %s" , get_next_line(fd));
+	printf("3 CALL: %s" , get_next_line(fd));
 	return (0);
 }
